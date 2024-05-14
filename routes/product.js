6c3ee8a4494a4f5896bc48ab353c7d7487d1b212
ansh <anshsachan421@gmail.com>
2024-05-14 20:29:56 +0530

    code refactor

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,34 @@ var router = express.Router();
 var User = require('../models/user');
 var Product = require('../models/product');
 
+// single user
+router.get('/:id', (req, res, next) => {
+  var { userId } = req.session;
+  var productId = req.params.id;
+  Product.findById(productId)
+    .then((product) => {
+      User.findById(userId)
+        .then((user) => res.render('singleUser', { product, user }))
+        .catch((err) => next(err));
+    })
+    .catch((err) => next(err));
+});
+
+// all product
+router.get('/', (req, res, next) => {
+  var { userId } = req.session;
+  Product.find({})
+    .then((products) => {
+      User.findById(userId)
+        .then((user) => {
+          return res.render('allProducts', { products, user });
+        })
+        .catch((err) => next(err));
+    })
+    .catch((err) => next(err));
+});
+
+// new product
 router.get('/new', (req, res, next) => {
   var { userId } = req.session;
   User.findById(userId)
@@ -24,13 +52,63 @@ router.post('/', (req, res, next) => {
     })
     .catch((err) => next(err));
 });
-router.get('/', (req, res, next) => {
+
+// edit form
+router.get('/:id/edit', (req, res, next) => {
+  var productId = req.params.id;
   var { userId } = req.session;
-  Product.find({})
-    .then((products) => {
-      User.findById(userId)
-        .then((users) => {
-          return res.render('allProducts', { products, users });
+  User.findById(userId)
+    .then((user) => {
+      if (user.isAdmin && user) return next(err);
+      Product.findById(productId)
+        .then((product) => res.render('edit', product))
+        .catch((err) => next(err));
+    })
+    .catch((err) => next(err));
+});
+
+router.post('/:id/edit', (req, res, next) => {
+  var { userId } = req.session;
+  var productId = req.params.id;
+  User.findById(userId).then((user) => {
+    if (!user || !user.isAdmin) return next(err);
+    Product.findByIdAndUpdate(productId, req.body, { new: true })
+      .then((product) => res.redirect('/products/' + productId))
+      .catch((err) => next(err));
+  });
+});
+
+// like
+router.get('/:id/like', (req, res, next) => {
+  var id = req.params.id;
+  Product.findByIdAndUpdate(id, { $inc: { like: 1 } })
+    .then((product) => res.redirect('/products/' + id))
+    .catch((err) => next(err));
+});
+
+// delete
+router.get('/:id/delete', (req, res, next) => {
+  var id = req.params.id;
+  Product.findByIdAndDelete(id)
+    .then((product) => res.redirect('/products/' + id))
+    .catch((err) => next(err));
+});
+
+// cart
+router.get('/:id/cart', (req, res, next) => {
+  var productId = req.params.id;
+  var { userId } = req.session;
+  User.findById(userId)
+    .then((user) => {
+      Product.findById(productId)
+        .then((product) => {
+          User.findByIdAndUpdate(
+            userId,
+            { $push: { cart: product.id } },
+            { new: true }
+          )
+            .then((users) => res.redirect('users/cart'))
+            .catch((err) => next(err));
         })
         .catch((err) => next(err));
     })
